da499aac7bc000a5d00b758a805959d28a8c263d
Dan Isla <disla@jpl.nasa.gov>
2016-01-09 15:27:31 -0800

    added ctrl-c trap

--- rpi-earthcam/start.sh
+++ rpi-earthcam/start.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+function ctrl_c() {
+        echo "** Trapped CTRL-C"
+        exit
+}
+
 URL=$1
 
 [[ -z $URL ]] && echo "USAGE: $0 <url>" && exit 1
 
 while true; do
     echo "`date`: Streaming $URL"
-    omxplayer -p -b "$URL"
+    #omxplayer -p -b "$URL"
     sleep 1
 done
