e971899e59f1952d8f767e4a8e9cd454f08993d2
Dan Isla <disla@jpl.nasa.gov>
2016-05-17 09:08:11 -0700

    update to dockerfunc

--- .dockerfunc.bash
+++ .dockerfunc.bash
@@ -3,6 +3,40 @@ docker-cleanup(){
   docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
 }
 
+function docker-machine-default() {
+  name=default
+  [[ "$(docker-machine status $name)" == "Stopped" ]] && docker-machine start $name
+  eval $(docker-machine env $name)
+}
+
+function docker-volume-du() {
+  VOL=$1
+  [[ -z $VOL ]] && echo "USAGE: docker-volume-du <volume name>" && return 1
+  docker run -it --rm -v ${VOL}:/data --entrypoint=du alpine:3.3 -sh /data
+}
+
+function docker-ui() {
+  docker run --name dockerui --restart always -d -p 9000:9000 --privileged -v /var/run/docker.sock:/var/run/docker.sock elegoev/dockerui:latest
+}
+
+function docker-swarm-ui() {
+  docker run -d -p 9001:3000 -v /var/run/docker.sock:/var/run/docker.sock --name dockerswarm-ui --restart always mlabouardy/dockerswarm-ui:1.0.0
+}
+
+function docker-avahi() {
+  AVAHI_HOST=$1
+  [[ -z $AVAHI_HOST ]] && echo "USAGE: docker-avahi <host name>" && return 1
+  docker run -d --name avahi-${AVAHI_HOST} --net host --restart always -e AVAHI_HOST=${AVAHI_HOST} danisla/avahi:latest
+}
+
+function docker-install-completion() {
+  files=(docker-machine docker-machine-wrapper docker-machine-prompt)
+  for f in "${files[@]}"; do
+    curl -L https://raw.githubusercontent.com/docker/machine/v$(docker-machine --version | tr -ds ',' ' ' | awk 'NR==1{print $(3)}')/contrib/completion/bash/$f.bash > `brew --prefix`/etc/bash_completion.d/$f
+  done
+   curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > `brew --prefix`/etc/bash_completion.d/docker-compose
+}
+
 function gen-ssl-cert() {
   cn=$1
   [[ -z "${cn}" ]] && echo "USAGE: gen-ssl-cert <cn>" && return 1
