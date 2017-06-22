c408ac1ec54bd135256a587fc1c1dc44ecaef338
Dan Isla <dan.isla@gmail.com>
2017-06-21 23:27:30 -0700

    update to cloudshell

--- cloudshell/bootstrap.sh
+++ cloudshell/bootstrap.sh
@@ -2,8 +2,6 @@
 
 set -xe
 
-apt-get update
-
 # Install named packages from packages.txt
 apt-get install -y -q $(grep -vE "^\s*#" /opt/cloudshell/packages.txt  | tr "\n" " ")
 
@@ -17,14 +15,20 @@ apt-get update && apt-get install -y docker-ce
 
 # Install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.14.0/docker-compose-Linux-x86_64 > /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+chmod ugo+rwx /usr/local/bin/docker-compose
 
 # Google Cloud SDK
-curl -sSL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-158.0.0-linux-x86_64.tar.gz | tar zxf -
+curl -sSL https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-160.0.0-linux-x86_64.tar.gz | tar zxf -
 ./google-cloud-sdk/install.sh
+echo y | /google-cloud-sdk/bin/gcloud components install alpha beta kubectl bigtable datalab cloud-datastore-emulator pubsub-emulator docker-credential-gcr
+chmod ugo+rw -R /google-cloud-sdk/
 
 # AWS Command Line Tools
 pip install --upgrade awscli
 
-# Cleanup
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+# MP3 Renaming tools
+for f in rename.sh m4a2mp3 MP3_FileRename_FromID3.py; do
+    curl -L https://raw.githubusercontent.com/danisla/dockerfiles/master/mp3tools/${f} > /usr/local/bin/${f}
+    chmod +x /usr/local/bin/${f}
+done
+pip install mutagen==1.23
