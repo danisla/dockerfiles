db097a5b99227397fc7c492f14f3bac747a750b8
Dan Isla <dan.isla@gmail.com>
2019-06-24 07:55:44 +0200

    pcoip-client sound support

--- pcoip-client/pcoip-client.sh
+++ pcoip-client/pcoip-client.sh
@@ -4,11 +4,16 @@ export DISPLAY=:1
 
 xhost +
 
+USER_ID=1000
+
 cd ${HOME}/projects/pcoip-client
 docker run -d --rm \
   -h ${HOSTNAME} \
   -v $(pwd)/.config/:/home/myuser/.config/Teradici \
   -v $(pwd)/.logs:/tmp/Teradici/$USER/PCoIPClient/logs \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -e DISPLAY=$DISPLAY danisla/pcoip-client:latest
-
+  -v /run/user/${USER_ID}/pulse:/run/user/${USER_ID}/pulse \
+  -e PULSE_SERVER=unix:/run/user/${USER_ID}/pulse/native \
+  -e DISPLAY=$DISPLAY \
+  -u ${USER_ID} \
+  danisla/pcoip-client:latest
